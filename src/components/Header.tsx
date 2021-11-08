e8fc06cd77e23c3e8b7bf10921c46cc75dff8339
Ricardo Balbi <rbalbi@gmail.com>
2021-11-08 14:42:15 -0300

    some layout improvements.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,28 +41,28 @@ export default function Header() {
       ) : (
         <>
           {athlete ? (
-            <button className={styles.athleteButton}>
-              <Link
-                href={{
-                  pathname: `https://strava.com/athletes/${athlete?.id}`,
-                }}
-                passHref
-              >
-                <a target='_blank' rel='noreferrer'>
-                  <div className={styles.athleteInfo}>
-                    <img
-                      className={styles.athleteAvatar}
-                      src={athlete?.profile}
-                      alt='Athlete Profile'
-                    />
-                    <span
-                      className={styles.athleteName}
-                    >{`${athlete?.firstname} ${athlete?.lastname}`}</span>
-                  </div>
-                </a>
-              </Link>
-            </button>
+            // <button className={styles.athleteButton}>
+            <Link
+              href={{
+                pathname: `https://strava.com/athletes/${athlete?.id}`,
+              }}
+              passHref
+            >
+              <a target='_blank' rel='noreferrer'>
+                <div className={styles.athleteInfo}>
+                  <img
+                    className={styles.athleteAvatar}
+                    src={athlete?.profile}
+                    alt='Athlete Profile'
+                  />
+                  <span
+                    className={styles.athleteName}
+                  >{`${athlete?.firstname} ${athlete?.lastname}`}</span>
+                </div>
+              </a>
+            </Link>
           ) : (
+            // </button>
             <span className={styles.spinnerLoading}>
               <PushSpinner size={20} loading={true} />
             </span>
