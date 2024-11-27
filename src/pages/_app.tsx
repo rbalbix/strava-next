b648738887eb2f4e78e9e9cbf9ab1c172891fb3a
Ricardo Balbi <rbalbi@gmail.com>
2024-11-27 12:16:20 -0300

    (feat) add vercel Web Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps, NextWebVitalsMetric } from 'next/app';
 import '../styles/globals.css';
-// import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   // console.log(metric);
