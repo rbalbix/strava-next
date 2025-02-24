c7b5a01379ad67609d16454876e4123cf4f0ae95
Ricardo Balbi <rbalbi@gmail.com>
2025-02-24 12:16:47 -0300

    (feat) pallete colors imporvement

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     turbo: {
+//       // ...
+//     },
+//   },
+// };
+
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+// module.exports = withBundleAnalyzer({});
+
+// export default nextConfig;
