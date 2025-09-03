ea956b90e63528960c1c7db4a4fa69ab3daa9b69
Ricardo Balbi <rbalbi@gmail.com>
2025-09-03 19:07:10 -0300

    (feat) improve some features

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const baseAppURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.PUBLIC_APP_URL
+    ? 'https://stuff-stat-preview.vercel.app'
     : 'http://localhost:3000';
 
 const baseStravaURL = 'https://www.strava.com';
